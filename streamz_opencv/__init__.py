8c13ced862edebcf377e912b50d965ef6091b751
Lukas Winkler <Lukas.Winkler@catalysts.cc>
2020-07-19 11:48:30 +0200

    First working impl

--- streamz_opencv/__init__.py
+++ streamz_opencv/__init__.py
@@ -1,19 +1,18 @@
 import cv2
-from streamz.core import Source, Stream
+from streamz import Source, Stream
 from tornado import gen
 
 
 @Stream.register_api(staticmethod)
 class from_opencv(Source):
     """ Stream data from opencv """
-    def __init__(self, src: str, poll_interval=0.1, start=False, **kwargs):
+    def __init__(self, src, poll_interval=0.01, start=False, **kwargs):
         self.src = src
         self.poll_interval = poll_interval
 
         super(from_opencv, self).__init__(ensure_io_loop=True, **kwargs)
 
         self.stopped = True
-        self.started = False
 
         if start:
             self.start()
@@ -21,23 +20,27 @@ class from_opencv(Source):
     def start(self):
         if self.stopped:
             self.stopped = False
-            self.started = True
             self.stream = cv2.VideoCapture(self.src)
             self.loop.add_callback(self.poll_opencv)
 
+    def stop(self):
+        if not self.stopped:
+            self.stream.release()
+            self.stream = None
+            self.stopped = True
+
     def do_poll(self):
+        if self.stopped:
+            return
         grabbed, frame = self.stream.read()
         if grabbed:
             return frame
 
     @gen.coroutine
     def poll_opencv(self):
-        while True:
+        while not self.stopped:
             frame = self.do_poll()
-            if frame:
+            if frame is not None:
                 yield self._emit(frame)
             else:
                 yield gen.sleep(self.poll_interval)
-            if self.stopped:
-                break
-        self.stream.release()
--- tests/test_webcam.py
+++ tests/test_webcam.py
@@ -1,11 +1,20 @@
+import pytest
 from streamz import Stream
 from tornado import gen
 
-from streamz_opencv import from_opencv
+from streamz_opencv import from_opencv  # NOQA
 
 
+@pytest.mark.gen_test
 def test_from_webcam():
-    stream = Stream.from_opencv("0", asynchronous=True)
+    stream = Stream.from_opencv(0, asynchronous=True)
     out = stream.sink_to_list()
+    assert len(out) == 0
     stream.start()
-    yield gen.sleep(0.1)
+    for _ in range(10):
+        yield gen.sleep(0.1)
+    
+    # Default poll interval is 0.1
+    # Should produce at least 10 frames in 1s
+    assert len(out) >= 10
+    stream.stop()
